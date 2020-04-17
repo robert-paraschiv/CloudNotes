9a6c550af656658060938bf4d9190f597d861f32
Rokudo <35243268+rokudoz@users.noreply.github.com>
2020-04-17 19:13:28 +0300

    added banner ad

--- app/src/main/java/com/rokudoz/cloudnotes/MainActivity.java
+++ app/src/main/java/com/rokudoz/cloudnotes/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     NavController navController;
+
+    private AdView mBannerAd;
 
 
 
@@ -22,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         if (navHostFragment != null) {
             navController = navHostFragment.getNavController();
         }
+
+        mBannerAd = findViewById(R.id.banner_adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("4129AB584AC9547A6DDCE83E28748843") // Mi 9T Pro
+                .addTestDevice("B141CB779F883EF84EA9A32A7D068B76") // RedMi 5 Plus
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+
+        mBannerAd.loadAd(adRequest);
+
     }
 
 
